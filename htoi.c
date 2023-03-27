6cfbc7490100c7b22bd28b3181622e9c93dac68e
John Thingstad <jpthing@online.no>
2023-03-27 16:14:09 +0200

    Merge branch 'main' of github.com:jthing/cuasm

--- htoi.c
+++ htoi.c
@@ -20,7 +20,7 @@ uint64_t htoi(const char *ptr)
 
     uint64_t value = 0;
     for (int i = 0; i < 16; i++) {
-        uint8_t ch = ptr[i] >= '0' && ptr[i] <= 9 ? ptr[i] - '0' : ptr[i] - 'A';
+        uint8_t ch = ptr[i] >= '0' && ptr[i] <= '9' ? ptr[i] - '0' : ptr[i] - 'A';
         value += ch;
         value <<= 4;
     }
