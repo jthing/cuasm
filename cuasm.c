8ee07dcda6e0d1863c544d0fe91727bdb1701054
John Thingstad <jpthing@online.no>
2023-03-30 21:14:23 +0200

    Merge branch 'main' of github.com:jthing/cuasm
    Signed-off-by: John Thingstad <jpthing@online.no>

--- cuasm.c
+++ cuasm.c
@@ -2,9 +2,41 @@
 #include <libgen.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <stdarg.h>
 #include "htoi.h"
 
 #define MAXLINE 1024
+
+char *Fgets(char *s, int size, FILE *stream)
+{
+    char *ret = fgets(s, size, stream);
+    if (ret == NULL && errno != 0) {
+        perror("fgets: ");
+        exit(errno);
+    }
+    return ret;
+}
+
+void Printf(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int ret = vprintf(fmt, ap);
+    if (ret < 0) {
+        perror("printf: ");
+        exit(errno);
+    }
+}
+
+void Fclose(FILE *file)
+{
+    int ret = fclose(file);
+    if (ret == EOF) {
+        perror("fclose: ");
+        exit(errno);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,13 +51,14 @@ int main(int argc, char *argv[])
     }
 
     char buf[MAXLINE + 1];
-    while (fgets(buf, MAXLINE + 1, in_file) != NULL) {
-        printf("%s = %lu\n",buf, htoi(buf));
+    while (Fgets(buf, MAXLINE + 1, in_file) != NULL) {
+        Printf("%s = %lu\n",buf, htoi(buf));
     }
 
     if (in_file != stdin) {
-        fclose(in_file);
+        Fclose(in_file);
     }
 
     return 0;
 }
+
